379f3bb9feb1ad0d976f8e2e78e4fd9f7b21c041
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-03-08 01:20:03 +0000

    css change to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header className="text-center text-gray-950 font-bold header">
       <h1 className="text-3xl pt-4">Kevin Chan</h1>
-      <section className="pt-5 pb-2">
+      <section className="pt-3 pb-2">
         <p className=" text-xl">Junior Software Developer</p>
         <p className="mb-3 text-xl">Location: London</p>
       </section>
