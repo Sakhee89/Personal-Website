0cf3e0787ce577d898c8b4e36690d49a958275cb
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-08-12 23:37:41 +0100

    add more projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,7 +3,7 @@ export const projectsData = [
     id: 1,
     title: "News Server FrontEnd",
     description:
-      "I'm excited to share my frontend project, created using JavaScript, React.js, and Bootstrap during my immersive journey at Northcoders Bootcamp. Introducing the app, this is akin to a dynamic news site, where users can explore a range of articles contributed by various users. With interactive features, users can engage by adding and removing comments, as well as voting on articles. Our platform offers navigation with filtering options based on topics and the ability to sort articles by comment count, votes, or date created, in both ascending and descending orders.",
+      "This News Server frontend project was created using JavaScript, React.js, and Bootstrap during my immersive journey at Northcoders Bootcamp. Introducing the app, this is akin to a dynamic news site, where users can explore a range of articles contributed by various users. With interactive features, users can engage by adding and removing comments, as well as voting on articles. Our platform offers navigation with filtering options based on topics and the ability to sort articles by comment count, votes, or date created, in both ascending and descending orders.",
     hosted: "https://sakhee-news.netlify.app/",
     techStacks: ["JavaScript", "React.js", "Bootstrap"],
     gitHub: "https://github.com/Sakhee89/fe-sakhee-news",
@@ -12,7 +12,7 @@ export const projectsData = [
     id: 2,
     title: "News Server BackEnd API",
     description:
-      "I'm delighted to introduce my backend project developed using Node.js and PostgreSQL during my immersive journey at Northcoders Bootcamp. The API project mirrors the functionality of real-world backend services, akin to platforms like Reddit, furnishing vital information to the frontend architecture.",
+      "The backend project for the News Server was developed using Node.js and PostgreSQL during my immersive journey at Northcoders Bootcamp. The API project mirrors the functionality of real-world backend services, akin to platforms like Reddit, furnishing vital information to the frontend architecture.",
     hosted: "https://sakhee-news.onrender.com/api",
     techStacks: ["JavaScript", "PSQL", "Node.js", "Express.js", "Jest"],
     gitHub: "https://github.com/Sakhee89/sakhee-server",
@@ -21,9 +21,27 @@ export const projectsData = [
     id: 3,
     title: "Knowledge Nest",
     description:
-      "Hello! I'm excited to share our team's project, Knowledge Nest, developed during our time at Northcoders. Knowledge Nest is a user-friendly platform designed to connect individuals seeking services with those offering them. Our front-end project, built with Vue.js, Tailwind, and TypeScript, features instant messaging and a post system for users to explore various skillsets. The login uses Firebase authentication, with the ability to sign in by google or email and password. The messaging functionality is also integrated with Firebase, that enables real-time communication among users.",
+      "This team's project, Knowledge Nest, was developed during our time at Northcoders. Knowledge Nest is a user-friendly platform designed to connect individuals seeking services with those offering them. Our front-end project, built with Vue.js, Tailwind, and TypeScript, features instant messaging and a post system for users to explore various skillsets. The login uses Firebase authentication, with the ability to sign in by google or email and password. The messaging functionality is also integrated with Firebase, that enables real-time communication among users.",
     hosted: "https://knowledge-nest-fe.vercel.app/",
     techStacks: ["TypeScript", "Vue.js", "Firebase", "Tailwind"],
     gitHub: "https://github.com/TomMirfin/knowledge-nest-FE",
+  },
+  {
+    id: 4,
+    title: "Hotel Booking",
+    description:
+      "Built a dynamic hotel booking platform using TypeScript, Next.js, and React. Integrated Sanity CMS for content management and Stripe for secure payments. Enabled user authentication with NextAuth.js and enhanced the UI with Tailwind CSS and real-time notifications via react-hot-toast. Implemented efficient data fetching with SWR and intuitive date selection using react-datepicker.",
+    hosted: "https://hotel-booking-xi-five.vercel.app/",
+    techStacks: ["TypeScript", "Next.js", "Sanity CMS", "Tailwind"],
+    gitHub: "https://github.com/Sakhee89/hotel-booking-site",
+  },
+  {
+    id: 5,
+    title: "Events Platform",
+    description:
+      "Developed a community events platform enabling businesses to create, share, and manage events with local members. Built using TypeScript, React.js, and Node.js, with MongoDB and Stripe integration. Implemented Google Calendar API for event syncing and ensured secure user authentication and payment processing. Designed a responsive, user-friendly interface and provided detailed documentation for hosting and local setup.",
+    hosted: "https://fe-events-platform.vercel.app/",
+    techStacks: ["TypeScript", "Next.js", "Sanity CMS", "Tailwind"],
+    gitHub: "https://github.com/Sakhee89/fe-events-platform",
   },
 ];
